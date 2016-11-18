375ece392bc4f0ebb2d0c8fd372e198c8e852382
Nay Zaw Oo <nayzawoo@yandex.com>
2016-11-18 17:03:20 +0630

    add scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,9 @@
 <html>
   <head>
     <title>Sample App</title>
-    <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{url('css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{url('css/spinkit.css')}}">
     <link rel="stylesheet" href="{{url('css/animate.min.css')}}">
-    <link rel="stylesheet" href="{{url('css/app.css')}}">
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
     <script>
       var globals = {!!json_encode([
         'apiRoot' => url('api')
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 var PROD = JSON.parse(process.env.PROD_ENV || '0')
 
 var entryProd = {
-  'js/bundle.min.js' : ['./client/index']
+  'css/style.min.css': './client/scss/main',
+  'js/bundle.min.js': ['./client/js/index']
 }
-var plugins = []
+
+var extractCSS = new ExtractTextPlugin('[name]', {
+  allChunks: true
+})
+
+var sassLoaders = [
+  'css-loader?importLoaders=1',
+  'postcss-loader?sourceMap=inline',
+  'sass-loader?sourceMap=inline'
+]
+
+if (PROD) {
+  sassLoaders = [
+    'css-loader',
+    'postcss-loader',
+    'sass-loader',
+  ]
+}
+
+var plugins = [
+  extractCSS
+]
 
 if (PROD) {
   plugins = plugins.concat([
     new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
+      compress: {
+        warnings: false
+      }
     }),
     new webpack.DefinePlugin({
       'process.env': {
@@ -23,7 +48,8 @@ if (PROD) {
 module.exports = {
   devtool: PROD ? '' : 'eval',
   entry: PROD ? entryProd : {
-    'js/bundle.js' : ['./client/index']
+    'css/style.css': './client/scss/main',
+    'js/bundle.js': ['./client/js/index']
   },
   output: {
     path: path.join(__dirname, 'public'),
@@ -36,10 +62,23 @@ module.exports = {
       test: /\.js|\.jsx$/,
       exclude: /(node_modules|bower_components)/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'client')
+      include: path.join(__dirname, 'client/js')
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style-loader', sassLoaders)
     }]
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+  postcss: function() {
+    var option = [require('autoprefixer')]
+    if (PROD) {
+      option.push(require('cssnano'))
+    }
+    return option
   },
+  sassLoader: {
+    includePaths: [path.join(__dirname, 'client/scss'), 'node_modules']
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss']
+  }
 }
